e79306db907814b9a519f484124c2af59b20c64d
Piotr Wrotny <piotrwro01@gmail.com>
2024-09-02 16:09:49 +0200

    WIP basic book structure

---
+++ DDD_Library.Domain/Abstractions/Entity.cs
@@ -0,0 +1,28 @@
+﻿namespace DDD_Library.Domain.Abstractions;
+
+public abstract class Entity
+{
+    private readonly List<IDomainEvent> _domainEvents = new();
+
+    protected Entity(Ulid id)
+    {
+        Id = id;
+    }
+
+    public Ulid Id { get; init; }
+
+    public IReadOnlyList<IDomainEvent> GetDomainEvents()
+    {
+        return _domainEvents.ToList();
+    }
+
+    public void ClearDomainEvents()
+    {
+        _domainEvents.Clear();
+    }
+
+    protected void RaiseDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents.Add(domainEvent);
+    }
+}
---
+++ DDD_Library.Domain/Books/Book.cs
@@ -0,0 +1,41 @@
+﻿using DDD_Library.Domain.Abstractions;
+
+namespace DDD_Library.Domain.Books;
+
+public sealed class Book : Entity
+{
+    private Book(Ulid id) : base(id)
+    {
+    }
+
+    public string ISBNNumber { get; private set; }
+
+    public string Author { get; private set; }
+    public string Title { get; private set; }
+    public string CoverUrl { get; private set; }
+    public string Description { get; private set; }
+
+    public string Form { get; private set; }
+    public string Launguage { get; private set; }
+    public int EditionNumber { get; private set; }
+    public string Publisher { get; private set; }
+    public DateOnly PublicationDate  { get; private set;}
+    public bool IsPartOfSeries { get; private set; }
+    public string? SeriesName { get; private set; }
+    public string Genre { get; private set; }
+
+    public decimal Rating { get; private set; }
+    public bool IsAvailable { get; private set; }
+
+    public DateTime RentalStartDate { get; private set; }
+    public DateTime? RentalPlanedEndDate { get; private set; }
+    public DateTime RentalEndDate { get; private set; }
+
+    public int HowManyTimesRented { get; private set; }
+
+
+
+
+
+
+}
